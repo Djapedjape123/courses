cd2720ed5a67c2a293d8dd111ee986da9baccb4b
Djapedjape123 <petafipedjaradic@gmail.com>
2026-06-12 16:17:53 +0200

    single

---
+++ app/courses/[slug]/page.tsx
@@ -0,0 +1,98 @@
+import { getCourseBySlug, getInstructorBySlug } from "@/lib/data"
+import Link from "next/link";
+import { notFound } from "next/navigation"
+
+export default async function page({
+    params
+}: {
+    params: Promise<{ slug: string }>
+}) {
+    const { slug } = await params
+
+    const course = getCourseBySlug(slug);
+
+    if (!course) notFound();
+
+    const instructor = getInstructorBySlug(course.instructorSlug)
+
+
+    return (
+        <section className="pad-section">
+            <div className="container">
+                <nav className="back-nav">
+                    <Link href="/courses">Back to Courses</Link>
+                </nav>
+
+                <div className="course-layout">
+                    <div>
+                        <div className="badge-row">
+                            <span className="badge">{course.category}</span>
+                            <span className="badge">{course.level}</span>
+                            <span className="badge">{course.duration}</span>
+                        </div>
+                    </div>
+
+                    <h1 className="title-display">{course.title}</h1>
+                    <p className="course-lede">{course.shortDescription}</p>
+
+                    <div className="text-block">
+                        <h2>About this course</h2>
+                        <p>{course.description}</p>
+                    </div>
+                    <div className="lession-block">
+                        <h2>What aru you learning</h2>
+                        <ol>
+                            {course.lessons.map((l) => (
+                                <li key={l}>
+                                    <span className="lesson-name">{l}</span>
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
+
+                    <aside className="course-aside">
+                        <div className="sidebar-card">
+                            <p className="sidebar-label">
+                                At a glance
+                            </p>
+
+                            <dl className="meta-grid">
+                                <div>
+                                    <dt>Level</dt>
+                                    <dd>{course.level}</dd>
+                                </div>
+                                <div>
+                                    <dt>Duration</dt>
+                                    <dd>{course.duration}</dd>
+                                </div>
+                                <div>
+                                    <dt>Category</dt>
+                                    <dd>{course.category}</dd>
+                                </div>
+                                <div>
+                                    <dt>Lesson</dt>
+                                    <dd>{course.lessonsCount}</dd>
+                                </div>
+                            </dl>
+                        </div>
+
+                        {instructor ? (
+                            <div className="sidebar-card">
+                                <p className="sidebar-label">Instructor</p>
+                                <p>{instructor.name}</p>
+
+                                <Link href={`instructors/${instructor.slug}`}
+                                  className="link-bred is-block"
+                                >View profil</Link>
+
+                            </div>
+                        ) : null}
+
+
+                    </aside>
+                </div>
+            </div>
+
+        </section>
+    )
+}
--- app/courses/courses-catalog-clinet.tsx
+++ app/courses/courses-catalog-clinet.tsx
@@ -1,53 +1,64 @@
 "use client"
 
-import { Course } from "@/lib/data"
+import { instructors,Course } from "@/lib/data"
 import Link from "next/link"
+
+ function instructorNameForCourse(course: Course): string {
+    return (
+        instructors.find((i) => i.slug === course.instructorSlug)?.name || "Unknown Instructor"
+    )
+   
+ 
+ }
 
 export default function CoursesCatalogClinet({
     courses,
     category
-}:{
-   courses: Course[],
-   category: String[]
+}: {
+    courses: Course[],
+    category: String[]
 }) {
     return (
         <div className="stack-md">
             <div className="grid-cards">
 
                 {courses.map((course) => (
-                    <Link 
+                    <Link
                         href={`/courses/${course.slug}`}
-                        key={course.id}>
-                          <p>{course.title}</p>
+                        key={course.id}
+                        className="card-link">
+
+                        <div className="badge-row">
+                            <span className="badge">{course.category}</span>
+                            <span className="badge ">{course.level}</span>
+                        </div>
+
+                        <h3 className="card-link-title">{course.title}</h3>
+                        <p>{course.shortDescription}</p>
+
+                        <dl className="card-meta">
+                            <div>
+                                <dt className="">lessons</dt>
+                                <dd>{course.lessonsCount}</dd>
+                            </div>
+
+                            <div>
+                                <dt className="">Ducation</dt>
+                                <dd>{course.duration}</dd>
+                            </div>
+
+                        </dl>
+
+                        <div className="card-footer">
+                            <span className="">BY {instructorNameForCourse(course)}</span>
+                            <span className="card-footer-cta">View Courses</span>
+                        </div>
+
 
                     </Link>
                 ))}
 
-                <Link href="#" className="card-link">
 
-                    <div className="badge-row">
-                        <span className="badge">Web developer</span>
-                        <span className="badge ">Beginer</span>
-                    </div>
-
-                    <h3 className="card-link-title">Next.js and Tailwind CSS</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, illo!</p>
-
-                    <dl className="card-meta">
-                        <div>
-                            <dt className="">lessons</dt>
-                            <dd>12 lessons</dd>
-                        </div>
-
-                        <div>
-                            <dt className="">Ducation</dt>
-                            <dd>5h</dd>
-                        </div>
-
-                    </dl>
-
-
-                </Link>
             </div>
 
         </div>
