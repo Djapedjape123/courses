8448dc0eebf4cc0214ee8a2b69a97ce47afad889
Djapedjape123 <petafipedjaradic@gmail.com>
2026-06-17 13:47:09 +0200

    slyg za insturkotre

--- app/instructors/page.tsx
+++ app/instructors/page.tsx
@@ -1,18 +1,58 @@
+import { getAllInstructors } from "@/lib/data";
 import Link from "next/link";
 
+function initials(name: string): string{
+  return name
+            .split(" ")
+            .map((part) => part[0])
+            .filter(Boolean)
+            .slice(0 , 2)
+            .join()
+            .toUpperCase()
+}
+
 export default function Instructors() {
+  const istructorsList = getAllInstructors()
+
+
+
   return (
-     <section className="pad-section">
+    <section className="pad-section">
       <div className="container">
         <div className="stack">
-            <div className="section-head">
-                <span className="eyebrow">Instructors</span>
-                <h2 className="title-page">All instructors</h2>
-                <p className="text-muted max-prose">Brows all my courses for web development</p>
-            </div>
+          <div className="section-head">
+            <span className="eyebrow">Instructors</span>
+            <h2 className="title-page">All instructors</h2>
+            <p className="text-muted max-prose">Brows all my courses for web development</p>
+          </div>
+
+          <div className="grid-cards">
+            {istructorsList.map((instructor) => (
+              <Link
+                key={instructor.id}
+                href={`/instructors/${instructor.slug}`}
+                className="card-link"
+              >
+                <div className="instructor-row">
+                  <div className="avatar">{initials(instructor.name)}</div>
+                  <div>
+                    <h1 className="instructor-card-name">{instructor.name}</h1>
+                    <p className="instructor-role">{instructor.specialty}</p>
+                  </div>
+                </div>
+
+                <p className="instructor-card-bio">{instructor.shortBio}</p>
+
+                <span className="instructor-card-cta">View Profil</span>
+
+              </Link>
+            ))}
+
+          </div>
 
 
-            
+
+
 
         </div>
       </div>
